c8a84711c469adf32d5e8cec494095402a992371
trishadd <89003949+trishadinwonderland@users.noreply.github.com>
2021-11-19 11:22:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -159,8 +159,28 @@ def event_handle(event):
             replyObj = TextSendMessage(text="ขี้เกียจ")
             line_bot_api.reply_message(rtoken, replyObj)
         else :
-            replyObj = TextSendMessage(text=msg)
-            line_bot_api.reply_message(rtoken, replyObj)
+           headers = request.headers
+           json_headers = json.dumps({k:v for k, v in headers.items()})
+           '''
+           json_line = request.get_json(force=False,cache=False)
+           json_line = json.dumps(json_line)
+           decoded = json.loads(json_line)
+           '''
+           crl= pycurl.Curl()
+           crl.setopt( crl.URL, "https://bots.dialogflow.com/line/s-a-rsau/webhook")
+           crl.setopt( crl.POST, 1)
+           #crl.setopt( crl.BINARYTRANSFER, true)
+           #crl.setopt( crl.POSTFIELDS, json_headers)
+           #crl.setopt( crl.HTTPHEADER, json_headers)
+           #crl.setopt( crl.SSL_VERIFYHOST, 2)
+           #crl.setopt( crl.SSL_VERIFYPEER, 1)
+           #crl.setopt( crl.FOLLOWLOCATION, 1)
+           #crl.setopt( crl.RETURNTRANSFER, 1)
+           crl.perform()
+           crl.close()
+
+           replyObj = TextSendMessage(text=json_headers)
+           line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
