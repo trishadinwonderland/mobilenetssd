78b394d6c7b0139cdb5e500fa432bd285cfead1e
trishadd <89003949+trishadinwonderland@users.noreply.github.com>
2021-12-03 12:03:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def event_handle(event,json_line):
         elif (msg == "ไปเที่ยวกัน") :
             replyObj = TextSendMessage(text="ขี้เกียจ")
             line_bot_api.reply_message(rtoken, replyObj)
-            elif msg == "covid" :
+        elif msg == "covid" :
             url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
             response = requests.get(url)
             response = response.json()
